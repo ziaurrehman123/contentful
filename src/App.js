347d86e3a5650d873419f1a7834bafcc2022046a
ziaurrehman123 <ziaurrehman1122111@gmail.com>
2021-05-05 17:01:23 +0500

    get data from contentful api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import { client } from "./client";
 
-function App() {
+const App=()=> {
+  const [meeting, setMeeting]=useState();
+  useEffect(()=>{
+    client.getEntries()
+    .then((response)=>{
+      console.log(response);
+      setMeeting({meeting:response.items})
+    })
+    .catch(console.error)
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Schedule Meet</h1>
     </div>
   );
 }
